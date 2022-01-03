42c3bf0f19620137296fd49c5340d53304ce9c15
0xfoursee <0xfoursee@gmail.com>
2022-01-03 09:29:53 +0000

    Adding vendor

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,25 +1,40 @@
 pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable{
 
   YourToken yourToken;
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
+  function buyTokens() public payable returns (bool) {
+    // msgs.value only used when dealing with eth balances
+    uint token_amount = (tokensPerEth * msg.value) / 1 ether;
+    yourToken.transfer(msg.sender, token_amount);
+    emit BuyTokens(msg.sender, msg.value, token_amount);
+    return true;
+  }
 
-  // ToDo: create a payable buyTokens() function:
 
+  function withdraw(uint256 amount) public onlyOwner returns (bool) {
+    payable(owner()).transfer(amount);
+    return true;
+  }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-
-  // ToDo: create a sellTokens() function:
-
+  function sellTokens(uint256 token_amount) public returns (bool) {
+    // requiresApproval-like check done inside ERC20.transferFrom()
+    yourToken.transferFrom(msg.sender, address(this), token_amount);
+    uint eth_amount = (token_amount * 1 ether) / tokensPerEth;
+    payable(msg.sender).transfer(eth_amount);
+    return true;
+  }
 
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -2,13 +2,11 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
-    // ToDo: add constructor and mint tokens for deployer,
-    //       you can use the above import for ERC20.sol. Read the docs ^^^
-
-     constructor() public ERC20("Gold", "GLD") {
-        // _mint() 1000 * 10 ** 18 to msg.sender
+     constructor() public ERC20("YourToken", "MYT") {
+       _mint(msg.sender, 1000*10**18);
     }
 }
